5562cf472b2ceaca78d3385c27df30d006d1ba18
Seonghun Kang <neuron03@gmail.com>
2017-07-20 17:15:47 +0900

    권한설정

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,5 @@
 class ImagesController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
   before_action :set_image, only: [:show, :edit, :update, :destroy]
   before_action :set_client
   before_action :set_os_collection
