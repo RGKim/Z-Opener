f506a916d1f47fe44eb7888174b9e503f8368b6f
Seonghun Kang <neuron03@gmail.com>
2017-07-20 15:46:36 +0900

    로그인 기능 구축 전 백업

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :images do
   resources :orders
   end
-  resources :devices
   resources :images
-  root 'welcome#index'
+  root 'devices#index'
   get '/order_page' => 'images#order_page'
+  get '/devices' => 'devices#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
